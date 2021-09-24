4c169a70c265bde4d1fc260e44ea923f1ccca4d5
Mohamed-Elhabashy <mohamedsamy01090@gmail.com>
2021-09-24 20:18:07 +0200

    Edit Create Form

--- Data/Models/Employee.cs
+++ Data/Models/Employee.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -9,17 +10,29 @@ namespace Mangement_System.Data.Models
     {
         
         public int EmployeeId { get; set; }
+        [Required]
         public string Name { get; set; }
+        [Required]
         public string country { get; set; }
+        [Required]
         public string Role { get; set; }
+        [Required]
         public string Areaofstudy { get; set; }
+        [Required]
         public string Phone { get; set; }
+        [Required]
         public string NationalId { get; set; }
+        [Required]
         public string Address { get; set; }
+        [Required]
         public string gender { get; set; }
+        [Required]
         public string Socialstatus { get; set; } // الحاله الاجتماعيه
+        [Required]
         public float Salary { get; set; }
+        [Required]
         public DateTime Birthdate { get; set; }
+        [Required]
         public DateTime JoinInStaff { get; set; }
     }
 }
